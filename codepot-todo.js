c0b5cd9c10fab10cd24394fe84eea22ab6d9f989
George Babey <gbabey@applause.com>
2015-08-27 15:08:41 +0200

    Switching to not using workshopper, just static files

--- codepot-todo.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const Workshopper = require('workshopper');
-
-Workshopper({
-  name: 'codepot-todo',
-  title: 'Codepot Todo 2015: Full Stack Javascript Development',
-  appDir: __dirname,
-  languages : ['en']
-});
