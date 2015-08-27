92b4bba1e5cd28f9edc757cb13939807ac7f12ae
renou <bjin@utest.com>
2015-08-27 16:15:38 +0200

    8000 -> 3000

--- 4_hello_world_server/solution/solution.js
+++ 4_hello_world_server/solution/solution.js
@@ -7,7 +7,7 @@ var server = http.createServer(function (request, response) {
   response.end();
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
+// Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(3000);
 
 // Put a friendly message on the terminal
